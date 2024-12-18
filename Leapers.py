083a127c0891a0c3d0d5d02bc750a6b99cb0c5ba
YeeeeeiAle <ale@grand.it>
2024-12-18 12:26:50 +0100

    Update Leapers.py

--- Leapers.py
+++ Leapers.py
@@ -53,6 +53,9 @@ elif nav == "Urbex":
     with st.container():
         st.write("---")
         st.title("Urbex :flashlight::derelict_house_building:")
+        st.subheader("About Urbex")
+        st.write("Urban exploration, often abbreviated as urbex, is the practice of exploring man-made structures, usually abandoned ruins or hidden components of the built environment.")
+        st.write("This activity can also involve visiting places that are off-limits or not easily accessible to the public, such as old factories, hospitals, tunnels, amusement parks, or historical buildings.")
         st.subheader("Abandoned Buildings")
         col1, col2, col3 = st.columns(3)
         with col1:
@@ -71,4 +74,3 @@ elif nav == "Urbex":
             st.image(piscina3, width=400)
         with col4:
             st.image(piscina4, width=400)
-
