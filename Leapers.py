15b4004bc17f9423f093567c636c407f280df8c6
YeeeeeiAle <ale@grand.it>
2024-12-19 18:25:01 +0100

    Update Leapers.py

--- Leapers.py
+++ Leapers.py
@@ -16,6 +16,11 @@ piscina4 = Image.open("Images/piscina4.jpg")
 #--------Pagina iniziale
 
 st.set_page_config(page_title="Leapers", page_icon=":heartpulse:", layout="wide")
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style.css")
 nav = option_menu(
     menu_title= "",
     options=["About", "Graffiti", "Urbex"],
@@ -56,21 +61,25 @@ elif nav == "Urbex":
         st.subheader("About Urbex")
         st.write("Urban exploration, often abbreviated as urbex, is the practice of exploring man-made structures, usually abandoned ruins or hidden components of the built environment.")
         st.write("This activity can also involve visiting places that are off-limits or not easily accessible to the public, such as old factories, hospitals, tunnels, amusement parks, or historical buildings.")
-        st.subheader("Abandoned Buildings")
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.image(palazzo1, width=400)
-        with col2:
-            st.image(palazzo2, width=400)
-        with col3:
-            st.image(palazzo3, width=700)
-        st.subheader("Abandoned Swimming Pools")
-        col1, col2, col3, col4 = st.columns(4)
-        with col1:
-            st.image(piscina1, width=400)
-        with col2:
-            st.image(piscina2, width=400)
-        with col3:
-            st.image(piscina3, width=400)
-        with col4:
-            st.image(piscina4, width=400)
+        st.title("PHOTOS")
+
+        with st.expander("Abandoned Buildings"):
+            st.subheader("Abandoned Buildings")
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.image(palazzo1, width=400)
+            with col2:
+                st.image(palazzo2, width=400)
+            with col3:
+                st.image(palazzo3, width=700)
+        with st.expander("Abandoned Swimming Pools"):
+            st.subheader("Abandoned Swimming Pools")
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:
+                st.image(piscina1, width=400)
+            with col2:
+                st.image(piscina2, width=400)
+            with col3:
+                st.image(piscina3, width=400)
+            with col4:
+                st.image(piscina4, width=400)
